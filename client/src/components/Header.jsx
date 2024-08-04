73d745e7df3d94eeda04d610a888bc22099e7145
EethakulaBhavani <eethakulabhavani@gmail.com>
2024-08-04 21:43:24 +0530

    completed sign-in page functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,22 +3,22 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
   return (
-    <header className='bg-red-200 shadow-lg'>
+    <header className='bg-orange-200 shadow-lg'>
     <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
         <h1 className='font-bold text-sm sm:text-2xl flex flex-wrap'>
-        <span className='text-rose-800'>Home</span>
-        <span className='text-red-600'>Haven</span>
+        <span className='text-amber-700'>Home</span>
+        <span className='text-amber-950'>Haven</span>
         </h1>
         </Link>
-        <form className='bg-red-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-orange-100 p-3 rounded-lg flex items-center'>
         <input type="text" placeholder="Search..." className='bg-transparent focus:outline-none w-24 sm:w-64'/>
         <FaSearch className='text-red-950'/>
         </form>
         <ul className='flex gap-5'>
-           <Link to='/'> <li className='font-semibold hidden sm:inline text-red-700 hover:underline'>Home</li> </Link> 
-           <Link to='/about'><li className='font-semibold hidden sm:inline text-red-700 hover:underline'>About</li> </Link>
-           <Link to='/sign-in'> <li className='font-semibold text-red-700 hover:underline'>Sign in</li> </Link>
+           <Link to='/'> <li className='font-semibold hidden sm:inline text-amber-900 hover:underline'>Home</li> </Link> 
+           <Link to='/about'><li className='font-semibold hidden sm:inline text-amber-900 hover:underline'>About</li> </Link>
+           <Link to='/sign-in'> <li className='font-semibold text-amber-900 hover:underline'>Sign in</li> </Link>
         </ul>
     </div>
       
