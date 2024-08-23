15fca97c61522726084d5c71894532e1f8313105
EethakulaBhavani <eethakulabhavani@gmail.com>
2024-08-23 15:35:14 +0530

    created search page UI

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+
+export default function 
+Search() {
+  return (
+    <div className='flex flex-col md:flex-row'>
+        <div className="p-7 border-b-2 md:border-r-2 md:min-h-screen">
+            <form className='flex flex-col gap-8'>
+                <div className="flex items-center gap-2">
+                    <label className='whitespace-nowrap font-semibold'>Search Here:</label>
+                    <input type='text' id='searchTerm' placeholder='Search .....'
+                    className='boorder rounded-lg p-3 w-full'/>
+                </div>
+                <div className="flex gap-2 flex-wrap items-center">
+                    <label className='font-semibold'>Type:</label>
+                    <div className="flex gap-2">
+                        <input type='checkbox' id='all' className='w-5'/>
+                        <span>Rent & Sale</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <input type='checkbox' id='rent' className='w-5'/>
+                        <span>Rent</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <input type='checkbox' id='sale' className='w-5'/>
+                        <span>Sale</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <input type='checkbox' id='offer' className='w-5'/>
+                        <span>Offer</span>
+                    </div>
+                </div>
+                <div className="flex gap-2 flex-wrap items-center">
+                    <label className='font-semibold'>Facilities:</label>
+                    <div className="flex gap-2">
+                        <input type='checkbox' id='parking' className='w-5'/>
+                        <span>Parking</span>
+                    </div>
+                    <div className="flex gap-2">
+                        <input type='checkbox' id='furnished' className='w-5'/>
+                        <span>Furnished</span>
+                    </div>
+                    
+                </div>
+                <div className="flex items-center gap-2">
+                    <label className='font-semibold'>Filter: </label>
+                    <select id='sort_order' className='border rounded-lg p-3'>
+                        <option>Price High to Low</option>
+                        <option>Price Low to High</option>
+                        <option>Latest</option>
+                        <option>Oldest</option>
+                    </select>
+                </div>
+                <button className='rounded-lg p-3 bg-amber-800 text-white hover:opacity-85 uppercase font-semibold'>Search</button>
+            </form>
+        </div>
+        <div className="">
+            <h1 className='text-3xl font-semibold p-3 border-b text-amber-800 mt-5 '>Here are your lists</h1>
+        </div>
+    </div>
+  )
+}
