ded73ce6843312ac08ac583218cdc4b1e5821c68
EethakulaBhavani <eethakulabhavani@gmail.com>
2024-08-26 20:22:47 +0530

    completed about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,6 +2,23 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl my-7 font-bold mb-4 text-lime-900'>About Home Haven</h1>
+      <p className='mb-5 text-gray-700'>At Home Haven, we are dedicated to providing exceptional real estate services that cater to the diverse needs of our clients. 
+        Whether you're buying, selling, or renting, our mission is to simplify the process and deliver results that exceed your expectations. 
+        With a commitment to integrity, transparency, and personalized service, we strive to make every real estate transaction a positive and seamless experience.</p>
+        <p className='mb-5 text-gray-700'>
+        Choosing Home Haven means choosing a partner who values your satisfaction above all else.
+        We pride ourselves on our customer-centric approach, ensuring that you receive personalized attention and guidance throughout your real estate journey.
+        Our innovative platform, built with the latest in real estate technology, provides you with up-to-date listings, market trends, and valuable resources, making your experience both efficient and enjoyable.
+        Let us help you turn your real estate dreams into reality.
+        </p>
+        <p className='mb-5 text-gray-700'>
+        We are committed to providing exceptional real estate services that cater to your unique needs.
+        Whether you're looking to buy, sell, or rent, our team of experienced professionals is here to guide you every step of the way.
+        With a deep understanding of the market and a focus on personalized service, we leverage the latest technology to deliver results that exceed your expectations.
+        Our goal is to make your real estate journey seamless, transparent, and successful, turning your property dreams into reality.
+        </p>
+    </div>
   )
 }
